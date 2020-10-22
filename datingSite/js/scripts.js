68cec92423109e93da63a3aa91ffcb3e920bfe23
Joey3001 <josephnero111@gmail.com>
2020-10-22 14:55:14 -0700

    Have a working logic branch & radio button form.
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>
    co-authored-by: Randel Moore <randel.c.moore@gmail.com>

---
+++ datingSite/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#hero").submit(function(event) {
+    let chosenColor = $('input:radio[name="color"]:checked').val();
+    let money = $('input:radio[name="money"]:checked').val();
+    alert("text");
+
+    if (chosenColor === "green" && money === "low") {
+      $("#hulk").show();
+    } else if (chosenColor === "red" && money === "high") {
+      $("#ironman").show();
+    } else { // if (chosenColor === "yellow" && money == "medium") {
+      $("#thor").show();
+    } 
+    console.log(money);
+  
+    event.preventDefault();
+  });
+});
